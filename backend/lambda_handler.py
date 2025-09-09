3df74a13da4b680f724850b1e65673507914fb53
Caroline Zhang <caroline.fzhang@gmail.com>
2025-09-09 23:15:24 +1000

    update lambda handler

--- backend/lambda_handler.py
+++ backend/lambda_handler.py
@@ -1,8 +1,8 @@
 from app import app
-import awsgi
+import awsgi2
 
 def lambda_handler(event, context):
     """AWS Lambda handler function"""
     
-    # Use awsgi to adapt the Flask app to the Lambda event
-    return awsgi.response(app, event, context)
+    # Use awsgi2 to adapt the Flask app to the Lambda event
+    return awsgi2.response(app, event, context, base64_content_types={"image/png"})
